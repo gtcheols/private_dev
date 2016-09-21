2199840963b22e963c8308299af72e491ad9011d
gtcheols <gtcheols@gmail.com>
2016-09-21 17:28:01 +0900

    Mod #1

--- JavaSimpleProject/src/Main.java
+++ JavaSimpleProject/src/Main.java
@@ -1,11 +1,16 @@
 
 public class Main {
 
+	private int val = 0;
+	
 	public Main() {
+		val = 1;
 	}
 	
+	public int getVal(){ return val; }
+	
 	public static void main(String[] args){
-		System.out.println("Hello JAVA.");
+		System.out.println("Hello JAVA." + (new Main()).getVal());
 	}
 
 }
